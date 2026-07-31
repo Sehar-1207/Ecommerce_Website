9a9ae1877b94d50ee834e8b6e86b5fc2972652e2
Sehar Ajmal <seharajmal452@gmail.com>
2026-07-31 11:29:05 +0500

    Image path

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,12 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
         hostname: "dummyjson.com",
         port: "",
         pathname: "/**",
@@ -15,13 +21,21 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "i.dummyjson.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:5000/api/:path*",
+        destination: process.env.NEXT_PUBLIC_API_URL
+          ? `${process.env.NEXT_PUBLIC_API_URL}/:path*`
+          : "http://localhost:5000/api/:path*",
       },
     ];
   },
